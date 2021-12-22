c63d66268eadd86bbef148b7fd0ce25636155545
CroissantAhhh <jasonz4@uw.edu>
2021-12-21 22:43:06 -0800

    side bar still WIP

--- react-app/src/components/SideBar/index.js
+++ react-app/src/components/SideBar/index.js
@@ -41,10 +41,10 @@ export default function SideBar() {
                         className="side-bar-nav-content l-horizontal"
                         style={{ backgroundColor: currentURL.includes("/home") ? "grey" : "black" }}
                         onClick={() => history.push('/home')}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="30px" height="30px" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                            <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
                         </svg>
-                        <p className="side-bar-nav-label">Home</p>
+                        <p className="side-bar-nav-label font-normal">Home</p>
                     </div>
                 </div>
                 <div className="side-bar-nav l-horizontal hover-pointer">
@@ -52,10 +52,10 @@ export default function SideBar() {
                         className="side-bar-nav-content l-horizontal"
                         style={{ backgroundColor: currentURL.includes("/my/media") ? "grey" : "black" }}
                         onClick={() => history.push('/my/media')}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="30px" height="30px" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                            <path d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z" />
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 4v16M17 4v16M3 8h4m10 0h4M3 12h18M3 16h4m10 0h4M4 20h16a1 1 0 001-1V5a1 1 0 00-1-1H4a1 1 0 00-1 1v14a1 1 0 001 1z" />
                         </svg>
-                        <p className="side-bar-nav-label">Media</p>
+                        <p className="side-bar-nav-label font-normal">Media</p>
                     </div>
                 </div>
                 <div className="side-bar-nav l-horizontal hover-pointer">
@@ -63,10 +63,10 @@ export default function SideBar() {
                         className="side-bar-nav-content l-horizontal"
                         style={{ backgroundColor: currentURL.includes("/my/artists") ? "grey" : "black" }}
                         onClick={() => history.push('/my/artists')}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="30px" height="30px" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                            <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd" />
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                         </svg>
-                        <p className="side-bar-nav-label">Artists</p>
+                        <p className="side-bar-nav-label font-normal">Artists</p>
                     </div>
                 </div>
                 <div className="side-bar-nav l-horizontal hover-pointer">
@@ -74,59 +74,66 @@ export default function SideBar() {
                         className="side-bar-nav-content l-horizontal"
                         style={{ backgroundColor: currentURL.includes("/my/albums") ? "grey" : "black" }}
                         onClick={() => history.push('/my/albums')}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="30px" height="30px" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                            <path d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z" />
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10" />
                         </svg>
-                        <p className="side-bar-nav-label">Albums</p>
+                        <p className="side-bar-nav-label font-normal">Albums</p>
                     </div>
                 </div>
             </div>
-            <div className="side-bar-divider"></div>
             <div className="side-bar-middle l-vertical">
                 <div className="side-bar-nav l-horizontal hover-pointer">
                     <div
                         className="side-bar-nav-content l-horizontal"
                         onClick={() => history.push('/home')}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="30px" height="30px" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                            <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z" clipRule="evenodd" />
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
                         </svg>
-                        <p className="side-bar-nav-label">Create Playlist</p>
+                        <p className="side-bar-nav-label font-normal">Create Playlist</p>
                     </div>
                 </div>
                 <div className="side-bar-nav l-horizontal hover-pointer">
                     <div
                         className="side-bar-nav-content l-horizontal"
                         onClick={() => history.push('/home')}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="30px" height="30px" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                            <path d="M18 3a1 1 0 00-1.196-.98l-10 2A1 1 0 006 5v9.114A4.369 4.369 0 005 14c-1.657 0-3 .895-3 2s1.343 2 3 2 3-.895 3-2V7.82l8-1.6v5.894A4.37 4.37 0 0015 12c-1.657 0-3 .895-3 2s1.343 2 3 2 3-.895 3-2V3z" />
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 19V6l12-3v13M9 19c0 1.105-1.343 2-3 2s-3-.895-3-2 1.343-2 3-2 3 .895 3 2zm12-3c0 1.105-1.343 2-3 2s-3-.895-3-2 1.343-2 3-2 3 .895 3 2zM9 10l12-3" />
                         </svg>
-                        <p className="side-bar-nav-label">Your Songs</p>
+                        <p className="side-bar-nav-label font-normal">Your Songs</p>
                     </div>
                 </div>
             </div>
-            <div className="side-bar-lower container l-vertical">
-                <div className="your-playlists">
-                    <p>Playlists</p>
-                </div>
+            <div className="side-bar-divider"></div>
+            <div className="side-bar-lower l-vertical">
                 <div className="playlist-tab-toggle l-horizontal">
-                    <div className="own-playlists" onClick={() => setPlaylistsTab(true)}>
-                        <p>My Playlists</p>
+                    <div
+                        className="playlist-tab l-horizontal hover-pointer"
+                        onClick={() => setPlaylistsTab(true)}
+                        style={{ backgroundColor: playlistsTab ? "grey" : "black" }}>
+                        <p className="playlist-tab-title font-normal">Created</p>
                     </div>
-                    <div className="followed-playlists" onClick={() => setPlaylistsTab(false)}>
-                        <p>Followed Playlists</p>
+                    <div
+                        className="playlist-tab l-horizontal hover-pointer"
+                        onClick={() => setPlaylistsTab(false)}
+                        style={{ backgroundColor: playlistsTab ? "black" : "grey" }}>
+                        <p className="playlist-tab-title font-normal">Followed</p>
                     </div>
                 </div>
                 <div className="playlist-container">
                     {playlistsTab ? (
                         ownPlaylists.map(playlist => (
-                            <div className="playlist-listing">
-                                {playlist.title}
+                            <div className="playlist-listing-container hover-pointer">
+                                <div className="playlist-listing">
+                                    <p className="playlist-listing-title font-narrow">{playlist.title}</p>
+                                </div>
                             </div>
                         ))
                     ) : (
                         followedPlaylists.map(playlist => (
-                            <div className="playlist-listing">
-                                {playlist.title}
+                            <div className="playlist-listing-container hover-pointer">
+                                <div className="playlist-listing">
+                                    <p className="playlist-listing-title font-narrow">{playlist.title}</p>
+                                </div>
                             </div>
                         ))
                     )}
